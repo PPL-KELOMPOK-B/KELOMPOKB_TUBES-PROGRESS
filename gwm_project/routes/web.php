8d5607703591bab0e314bfdb08e94ca183e239fe
MSI\user <naufal.firul.yuska@gmail.com>
2026-05-06 22:07:03 +0700

    SCRUM-81 Memperbaiki Fitur Login

--- gwm_project/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/dashboard', [DashboardController::class, 'index']);
