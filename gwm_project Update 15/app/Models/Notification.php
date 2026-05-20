1f7e580fbe6f64fa68ba08ed2c420a08e78a86a4
christopherfdarren195 <darrenmanalu0109@gmail.com>
2026-05-20 19:00:06 +0700

    PBI 6 - Notifikasi Perubahan Status Laporan

---
+++ gwm_project Update 15/app/Models/Notification.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    protected $table = 'notifications';
+
+    protected $fillable = [
+        'user_id',
+        'laporan_id',
+        'title',
+        'message',
+        'status',
+        'is_read',
+    ];
+
+    protected $casts = [
+        'is_read' => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function laporan()
+    {
+        return $this->belongsTo(Laporan::class);
+    }
+}
